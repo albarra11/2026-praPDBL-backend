ccad667c3287e7ec3719b2b0189671054e83fb28
albarra11 <ahmadhaqalbarra@gmail.com>
2026-02-16 00:49:07 +0700

    feat: add peminjaman seed data

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -18,6 +18,47 @@ namespace PinjamRuanganAPI.Data
                     .HasConversion<string>();
 
                 entity.HasQueryFilter(p => p.DeletedAt == null);
+                entity.HasData(
+                    new Peminjaman
+                    {
+                        Id = 1,
+                        NamaPeminjam = "Budi",
+                        NomorPeminjam = "081234567890",
+                        AlasanPeminjaman = "Rapat UKM",
+                        NamaRuangan = "A101",
+                        Tanggal = new DateTime(2026, 2, 15),
+                        WaktuMulai = new TimeSpan(9, 0, 0),
+                        WaktuSelesai = new TimeSpan(11, 0, 0),
+                        Status = Enums.StatusPeminjaman.Menunggu,
+                        DeletedAt = null
+                    },
+                    new Peminjaman
+                    {
+                        Id = 2,
+                        NamaPeminjam = "Siti",
+                        NomorPeminjam = "089876543210",
+                        AlasanPeminjaman = "Kelas tambahan",
+                        NamaRuangan = "B202",
+                        Tanggal = new DateTime(2026, 2, 16),
+                        WaktuMulai = new TimeSpan(13, 0, 0),
+                        WaktuSelesai = new TimeSpan(15, 0, 0),
+                        Status = Enums.StatusPeminjaman.Disetujui,
+                        DeletedAt = null
+                    },
+                    new Peminjaman
+                    {
+                        Id = 3,
+                        NamaPeminjam = "Andi",
+                        NomorPeminjam = "087700112233",
+                        AlasanPeminjaman = "Seminar",
+                        NamaRuangan = "A101",
+                        Tanggal = new DateTime(2026, 2, 17),
+                        WaktuMulai = new TimeSpan(8, 0, 0),
+                        WaktuSelesai = new TimeSpan(10, 0, 0),
+                        Status = Enums.StatusPeminjaman.Ditolak,
+                        DeletedAt = null
+                    }
+                );
             });
         }
     }
---
+++ Migrations/20260215174804_SeedPeminjamanData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace PinjamRuanganAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedPeminjamanData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Peminjaman",
+                columns: new[] { "Id", "AlasanPeminjaman", "DeletedAt", "NamaPeminjam", "NamaRuangan", "NomorPeminjam", "Status", "Tanggal", "WaktuMulai", "WaktuSelesai" },
+                values: new object[,]
+                {
+                    { 1, "Rapat UKM", null, "Budi", "A101", "081234567890", "Menunggu", new DateTime(2026, 2, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 9, 0, 0, 0), new TimeSpan(0, 11, 0, 0, 0) },
+                    { 2, "Kelas tambahan", null, "Siti", "B202", "089876543210", "Disetujui", new DateTime(2026, 2, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 13, 0, 0, 0), new TimeSpan(0, 15, 0, 0, 0) },
+                    { 3, "Seminar", null, "Andi", "A101", "087700112233", "Ditolak", new DateTime(2026, 2, 17, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 8, 0, 0, 0), new TimeSpan(0, 10, 0, 0, 0) }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Peminjaman",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Peminjaman",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Peminjaman",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
